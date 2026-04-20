a3374a9623af00d3b72a15df6377509c36dabbcc
Lia Fitria binti Mohd Adam Ho <liafitriaho@gmail.com>
2026-04-20 19:45:56 +0800

    tested and changed the screen size

--- Lia's codes/ui_layout.py
+++ Lia's codes/ui_layout.py
@@ -1,8 +1,11 @@
+from cmath import rect
+
 import pygame
 import os
 
 BASE_DIR = os.path.dirname(__file__)
 ASSETS_PATH = os.path.join(BASE_DIR,'..', 'Assets')
+WIDTH, HEIGHT = 1920, 1200
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -11,10 +14,10 @@ def load_assets():
     assets = {}
     try:
         logo = pygame.image.load(os.path.join(ASSETS_PATH, 'Game Logo.png')).convert_alpha()
-        assets['logo'] = pygame.transform.smoothscale(logo, (300, 300))
+        assets['logo'] = pygame.transform.smoothscale(logo, (600, 600))
 
         start_img = pygame.image.load(os.path.join(ASSETS_PATH, 'Start button.png')).convert_alpha()
-        assets['start_button'] = pygame.transform.smoothscale(start_img, (220, 80))
+        assets['start_button'] = pygame.transform.smoothscale(start_img, (400, 150))
 
         return assets
     except Exception as e:
@@ -22,20 +25,22 @@ def load_assets():
         return None
 
 def draw_button_img(screen, rect, image, is_hovered):
-    screen.blit(image, (rect.x, rect.y))
-
     if is_hovered:
-        glow_surface = pygame.Surface((rect.width, rect.height), pygame.SRCALPHA)
-        glow_surface.fill((255, 255, 255, 100))
-        screen.blit(glow_surface, (rect.x, rect.y))
-
+        surf = pygame.Surface(image.get_size(), pygame.SRCALPHA)
+        surf.set_alpha(150)  
+        surf.blit(image, (0, 0))
+        screen.blit(surf, (rect.x, rect.y))
+    else:
+        screen.blit(image, (rect.x, rect.y))
+    
 def test_ui():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((1920, 1200))
     pygame.display.set_caption("A Scoop of Spring - UI Test")
     all_assets = load_assets()
-    start_button_rect = pygame.Rect(290,400,220,80)
-    
+    start_button_rect = pygame.Rect(760, 700, 400, 150)
+    logo_position = (660, 150)
+
     clock = pygame.time.Clock()
     running = True
     while running:
@@ -45,7 +50,7 @@ def test_ui():
 
         if all_assets:
             if 'logo' in all_assets:
-                screen.blit(all_assets['logo'], (250, 50))
+                screen.blit(all_assets['logo'], logo_position)
 
             if 'start_button' in all_assets:
                 draw_button_img(screen, start_button_rect, all_assets['start_button'], is_hovered)
