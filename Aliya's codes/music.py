d0f872e3fe03d614895268a9306ddd50ae2d9c1d
aliayay <aliyayasmin07@gmail.com>
2026-05-25 13:07:39 +0800

    added logics for sound

---
+++ Aliya's codes/music.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.mixer.init()
+
+current_music = None
+
+
+def play_music(file, volume=0.4, loop=True):
+    global current_music
+
+    if current_music != file:
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load(file)
+        pygame.mixer.music.set_volume(volume)
+
+        loops = -1 if loop else 0
+        pygame.mixer.music.play(loops)
+
+        current_music = file
+
+
+def stop_music():
+    pygame.mixer.music.stop()
