0fade86a7dcb85869db38a7f44a792d33a05c6e0
aliayay <aliyayasmin07@gmail.com>
2026-06-08 10:53:22 +0800

    added sound,py

---
+++ Aliya's code/sound.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.mixer.init()
+
+current_music = None
+
+
+def play_music(file, volume=0.4, loop=True):
+    global current_music
+
+    if current_music != file:
+        pygame.mixer.music.stop()
+        print("Loading:", file)
+        pygame.mixer.music.load(file)
+        pygame.mixer.music.set_volume(volume)
+
+        loops = -1 if loop else 0
+        pygame.mixer.music.play(loops)
+
+        current_music = file
+
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+def play_sound_effect(file, volume=0.6):
+    sound = pygame.mixer.Sound(file)
+    sound.set_volume(volume)
+    sound.play()
